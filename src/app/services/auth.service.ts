8245f4647cc01fc7d911179a16377b4ade47069d
MarkBramblett18 <markbramblett18@gmail.com>
2022-10-09 23:39:57 -0400

    implement auth services

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  userLoggedIn: boolean; //lets components check if user is loggedIn
+
+  constructor(private router: Router, private afAuth: AngularFireAuth) {
+    this.userLoggedIn = false;
+
+    this.afAuth.onAuthStateChanged((user) => {              // set up a subscription to always know the login status of the user
+        if (user) {
+            this.userLoggedIn = true;
+        } else {
+            this.userLoggedIn = false;
+        }
+    });
+  }
+
+  loginUser(email: string, password: string): Promise<any> {
+    return this.afAuth.signInWithEmailAndPassword(email, password)
+        .then(() => {
+            console.log('Auth Service: loginUser: success');
+            // this.router.navigate(['/dashboard']);
+        })
+        .catch(error => {
+            console.log('Auth Service: login error...');
+            console.log('error code', error.code);
+            console.log('error', error);
+            if (error.code)
+                return { isValid: false, message: error.message };
+        });
+}
+
+signupUser(user: any): Promise<any> {
+    return this.afAuth.createUserWithEmailAndPassword(user.email, user.password)
+        .then((result) => {
+            let emailLower = user.email.toLowerCase();
+            result.user.sendEmailVerification();                    // immediately send the user a verification email
+        })
+        .catch(error => {
+            console.log('Auth Service: signup error', error);
+            if (error.code)
+                return { isValid: false, message: error.message };
+        });
+}
+
 }
