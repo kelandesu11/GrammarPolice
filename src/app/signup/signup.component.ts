1a1186886d28f39d7f34361f38a2b4cd2db545a2
MarkBramblett18 <markbramblett18@gmail.com>
2022-10-09 23:48:50 -0400

    implement basic signup features

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service'
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -7,9 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup;
+  firebaseErrorMessage: string;
 
-  ngOnInit(): void {
-  }
+  constructor(private authService: AuthService, private router: Router,
+     private afAuth: AngularFireAuth)
+   { this.firebaseErrorMessage = '';}
+
+   ngOnInit(): void {
+    this.signupForm = new FormGroup({
+        'displayName': new FormControl('', Validators.required),
+        'email': new FormControl('', [Validators.required, Validators.email]),
+        'password': new FormControl('', Validators.required)
+    });
+}
+
+signup() {
+    if (this.signupForm.invalid)                            // if there's an error in the form, don't submit it
+        return;
+
+    this.authService.signupUser(this.signupForm.value).then((result) => {
+        if (result == null)                                 // null is success, false means there was an error
+            this.router.navigate(['/dashboard']);
+        else if (result.isValid == false)
+            this.firebaseErrorMessage = result.message;
+    }).catch(() => {
+
+    });
+}
+
 
 }
